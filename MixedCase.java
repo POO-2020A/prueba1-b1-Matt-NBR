84e7a2455a92a2224a4a27040a88d92fe1411d7c
Matt-NBR <66144847+Matt-NBR@users.noreply.github.com>
2020-07-06 11:14:11 -0500

    Add files via upload

--- MixedCase.java
+++ MixedCase.java
@@ -27,10 +27,18 @@ aCrObAtIcS
 PISTA: Necesitaras una sentencia if dentro del for! 
 */
 class MixedCase {
-  public static void main(String[] args) {
-    Scanner inp = new Scanner(System.in);
-    System.out.print("In:");
-    String s = inp.nextLine();
-    //write your code below
-  }
+
+    public static void main(String[] args) {
+        Scanner inp = new Scanner(System.in);
+        System.out.print("In:");
+        String s = inp.nextLine();
+        //write your code below
+        for (int i = 0; i < s.length(); i++) {
+            if (i % 2 != 0) {
+                System.out.print(s.substring(i, i + 1).toUpperCase());
+            } else {
+                System.out.print(s.substring(i, i + 1).toLowerCase());
+            }
+        }
+    }
 }
